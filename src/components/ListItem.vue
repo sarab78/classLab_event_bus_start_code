e6210742d615a153a35ee85b673717c3673f94ad
sarab78 <sarbat_kaur@hotmail.co.uk>
2019-06-19 16:02:34 +0100

    classLab_w7d3

---
+++ src/components/ListItem.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <li v-on:click="handleClick">{{munro.name}}</li>
+</template>
+
+<script>
+import { eventBus } from '../main.js' //curly braces because we only import eventbus not whole main.bus
+export default {
+  name: 'list-item',
+  props: ['munro'],
+  methods: {
+    handleClick(){
+      eventBus.$emit('munro-selected', this.munro)//$emit is a function.
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/MunroeDetail.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div v-if='munro'>
+<h3>{{munro.name}}</h3>
+<p>Height: {{munro.height}}</p>
+<p>Region: {{munro.region}}</p>
+<p>Meaning: {{munro.meaning}}</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'munro-detail',
+  props: ['munro']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
